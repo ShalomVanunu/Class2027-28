e6d186b6914c0bab9c1377e34c4758235b6e4ce3
ShalomV <shalomvanunu@gmail.com>
2026-01-21 18:37:23 +0200

    21/1/26

---
+++ GroupA/prompt.py
@@ -0,0 +1,17 @@
+from dotenv import  load_dotenv
+from google import genai
+
+
+load_dotenv()
+
+client = genai.Client()
+
+response = client.models.generate_content(
+    model = "gemini-2.5-flash",
+    contents="explain in one sentence what is electric car"
+)
+
+print(response.text)
+
+
+
