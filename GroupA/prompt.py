3acc6c3ecbacacda77d83fbe3a9e0a5511f89b5c
ShalomV <shalomvanunu@gmail.com>
2026-01-28 18:46:31 +0200

    21/1/26

--- GroupA/prompt.py
+++ GroupA/prompt.py
@@ -1,17 +1,26 @@
-from dotenv import  load_dotenv
+
+
+from dotenv import load_dotenv
 from google import genai
-
-
 load_dotenv()
 
-client = genai.Client()
+## model = gemma-3-1b-it
+## model = gemma-3-4b-it
+## model = gemma-3-12b-it
+## model = gemma-3-27b-it
 
-response = client.models.generate_content(
-    model = "gemini-2.5-flash",
-    contents="explain in one sentence what is electric car"
-)
+def ask_gemini(prompt):
+    client = genai.Client()
+    response = client.models.generate_content(
+        model="gemma-3-1b-it",
+        contents=prompt
+    )
+    print(response.text)
 
-print(response.text)
-
+while True:
+    prompt = input("what do you want to know? (Q fo Quit) ")
+    if prompt.upper()=="Q":
+        break
+    ask_gemini(prompt)
 
 
